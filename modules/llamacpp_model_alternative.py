4e514624f9ea079f755141373cc5e7074f2d4a0c
Junghyun Kim <101810598+Zolyer@users.noreply.github.com>
2023-04-12 14:46:24 +0900

    add stop for llamacpp

--- modules/llamacpp_model_alternative.py
+++ modules/llamacpp_model_alternative.py
@@ -11,7 +11,6 @@ from llama_cpp import Llama
 from modules import shared
 from modules.callbacks import Iteratorize
 
-
 class LlamaCppModel:
     def __init__(self):
         self.initialized = False
@@ -23,6 +22,8 @@ class LlamaCppModel:
         params = {
             'model_path': str(path),
             'n_ctx': 2048,
+            'n_parts': 1,
+            'n_batch': 2048,
             'seed': 0,
             'n_threads': shared.args.threads or None
         }
@@ -36,7 +37,12 @@ class LlamaCppModel:
             string = string.encode()
         return self.model.tokenize(string)
 
-    def generate(self, context="", token_count=20, temperature=1, top_p=1, top_k=50, repetition_penalty=1, callback=None):
+    def generate(self, context="", token_count=20, temperature=0.1, top_p=0.1, top_k=40, repetition_penalty=1.176, callback=None, stop=[' ]', '### Human:']):
+        if stop != []:
+            stop_sequences = [s.encode("utf-8") for s in stop]
+        else:
+            stop_sequences = []
+        
         if type(context) is str:
             context = context.encode()
         tokens = self.model.tokenize(context)
@@ -45,6 +51,14 @@ class LlamaCppModel:
         count = 0
         for token in self.model.generate(tokens, top_k=top_k, top_p=top_p, temp=temperature, repeat_penalty=repetition_penalty):
             text = self.model.detokenize([token])
+            
+            any_stop = [s for s in stop_sequences if s in text]
+            if len(any_stop) > 0:
+                first_stop = any_stop[0]
+                text = text[: text.index(first_stop)]
+                finish_reason = "stop"
+                break
+            
             output += text
             if callback:
                 callback(text.decode())
